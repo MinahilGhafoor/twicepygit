99c8adefc00962fdf369e0e6b647f1381db2a57c
Minahil Ghafoor <ReadyForIt7890@gmail.com>
2025-03-01 20:20:53 +0500

    Colorful Outputs

--- Colors.py
+++ Colors.py
@@ -1,1 +1,42 @@
-# COLORFUL OUTPUTS
+# COLORFUL OUTPUT
+
+
+class Color:
+    # Text Colors
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+
+    # Background Colors
+    BLACK_BG = "\033[40m"
+    RED_BG = "\033[41m"
+    GREEN_BG = "\033[42m"
+    YELLOW_BG = "\033[43m"
+    BLUE_BG = "\033[44m"
+    MAGENTA_BG = "\033[45m"
+    CYAN_BG = "\033[46m"
+    WHITE_BG = "\033[47m"
+
+    # Style Codes
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    INVERSE = "\033[7m"
+    HIDDEN = "\033[8m"
+
+    # Reset Code
+    RESET = "\033[0m"
+
+    FONT_SIZE_SMALL = "\033[2m"  # not supported in all terminals
+    FONT_SIZE_LARGE = "\033[1m"  # not supported in all terminals
+
+
+
+
